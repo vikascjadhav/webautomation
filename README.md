a72d068fdc8eb4c85d115bd61d4083b75593a312
Vikas <vikascjadhav@gmail.com>
2016-07-06 19:30:04 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# webautomation
+#WebAutomation using selenium and java
+
+#### Download and Copy Webdriver location 
+  *        System.setProperty("webdriver.chrome.driver", "/home/vikas/workspace/webdriver/chromedriver");
+
+#### Run program using below command
+  *        $ mvn exec:java  -Dexec.mainClass="com.web.App"  
+     
---
+++ src/main/java/com/web/App.java
@@ -0,0 +1,46 @@
+package com.web;
+
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        System.setProperty("webdriver.chrome.driver", "/home/vikas/workspace/webdriver/chromedriver");
+        WebDriver driver = new ChromeDriver();        
+        driver.get("https://www.google.co.in");
+        WebElement element = driver.findElement(By.className("gb_pc"));
+        System.out.println("*********************************"+element.getText()+"**********************************");
+        element.click();
+        System.out.println("Waiting for some time");
+        Thread.sleep(4000);
+        element.click();
+
+        element = driver.findElement(By.id("q"));
+        element.sendKeys("Sellenium");
+        
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("Sellenium");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        //Close the browser
+        driver.quit();
+    }
+}
